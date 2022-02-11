d5a0604b99ad66b42e64b356ca2b78ff629896a1
Amit Deb <deb@sonoma.edu>
2022-02-10 21:19:56 -0800

    Updated test.py
    did first two parts of PArt 2

--- Project01/Project01a/test.py
+++ Project01/Project01a/test.py
@@ -8,3 +8,24 @@
 #plt.plot(x, y, 'o', color='black');
 #plt.show() 
 
+#Task Two Expontiation
+# i) Decrease by one
+
+def decreaseByOne(a,n):
+    if n == 0:
+        return 1
+    if n > 0:
+        return a * decreaseByOne(a, n-1)
+
+# i)
+
+def decreaseByConstantFactor(a, n):
+    if n == 0:
+        return 1
+    if n % 2 == 0:
+        return decreaseByConstantFactor(a, n/2) * decreaseByConstantFactor(a, n/2)
+    if n % 2 == 1:
+        return a * (decreaseByConstantFactor(a,(n-1)/2) * decreaseByConstantFactor(a,(n-1)/2))
+
+
+print(decreaseByConstantFactor(2, 4))
