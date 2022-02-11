3d63bbeec80f46ef49c79af9182f1593f3d22cba
Amit Deb <deb@sonoma.edu>
2022-02-11 03:31:02 -0800

    Task Three 01a-algos
    NOT TESTED!

--- Project01/Project01a/TaskThreeA.py
+++ Project01/Project01a/TaskThreeA.py
@@ -1,3 +1,32 @@
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import numpy as np
+
+print("File to Sort:")
+file = input()
+datafile = open(file, "r")
+#idk if file has bad open exceptions
+
+def selectionSort(data):
+    
+    comp = 0
+    for i in range(len(data)):
+        min = i
+        for j in range(i+1, len(data)):
+            if(data[j]<data[min]):
+                min = j
+                comp = comp + 1
+    return comp
+
+def insertionSort(data):
+    comp = 0
+    for i in range(1, len(data)):
+        find = data[i]
+        
+        j = i - 1
+        while j >= 0 and find < data[j]:
+            data[j+1] = data[j]
+            j -= 1
+            comp = comp + 1
+        data[j+1] = find
+    return comp
