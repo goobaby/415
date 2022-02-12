fe57fb206a9471288ff97dc73f2cc078c868165f
Amit Deb <deb@sonoma.edu>
2022-02-12 02:14:36 -0800

    Added User and Scatter to Task TwoA.py

--- Project01/Project01a/TaskTwoA.py
+++ Project01/Project01a/TaskTwoA.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import numpy as np
+import matplotlib.patches as mpatches
 
-#x = np.linspace(0, 10, 6)
-#y = np.sin(x)
+def setupLegend(title, xlabel, ylabel):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    redlegend = mpatches.Patch(color='red', label='Divide And Conquer')
+    bluelegend = mpatches.Patch(color='blue', label='Decrease By Constant Factor')
+    greenlegend = mpatches.Patch(color='green', label='Decrease By One')
+    plt.legend(handles=[redlegend, bluelegend, greenlegend])
 
-#plt.plot(x, y, 'o', color='black');
-#plt.show() 
 
 #Task Two Expontiation
 # i) Decrease by one
@@ -42,29 +47,43 @@ def divideAndConquer(a, n, x):
         tuple = decreaseByConstantFactor(a,(n-1)/2, x+1)
         return (a * tuple[0] * tuple[0], tuple[1]+2)
 
-n = 10
+n = 100
 base = 2
 counter = 0
 
-#one = decreaseByOne(2, 4, 0)
-#factor = decreaseByConstantFactor(2, 4, 0)
-#conquer = divideAndConquer(2, 4, 0)
+print("Type User or Scatter for mode:")
+mode = input()
+
+if mode == "user" or mode == "User":
+    print("Give value of 'a' ie(base of exponent)")
+    a = input()
+    print("Give value of 'n' ie(the exponent)")
+    n = input()
+    one = decreaseByOne(int(a), int(n), 0)
+    factor = decreaseByConstantFactor(int(a), int(n), 0)
+    conquer = divideAndConquer(int(a), int(n), 0)
+    
+    print("Decrease By One:" + str(one[0]))
+    print("Decrease By Constant Factor:" + str(factor[0]))
+    print("Divide And Conquer:" + str(conquer[0]))
+
+
 
 #alpha is to deal with overlaping issue
 #either make seperate graphs or do this
-for arg in list(range(n)):
-    plt.plot(arg, decreaseByOne(base, arg, counter)[1], 'gs', alpha=0.75)
-    plt.plot(arg, decreaseByConstantFactor(base, arg, counter)[1], 'bo', alpha=0.75)
-    plt.plot(arg, divideAndConquer(base, arg, counter)[1], 'r^', alpha=0.75)
+if mode == "scatter" or mode == "Scatter":
+    for arg in list(range(n)):
+        plt.plot(arg, decreaseByOne(base, arg, counter)[1], 'gs', alpha=0.75)
+        plt.plot(arg, decreaseByConstantFactor(base, arg, counter)[1], 'bo', alpha=0.75)
+        plt.plot(arg, divideAndConquer(base, arg, counter)[1], 'r^', alpha=0.75)
 
-plt.title("Exponentiation")
-plt.xlabel("Exponent Value")
-plt.ylabel("Number of Muliplications")
-#plt.legend(loc="lower right")
-#plt.scatter(one)
-#plt.scatter(factor)
-#plt.scatter(conquer)
-plt.show()
+    setupLegend("Exponentiation","Exponent Value","Number of Muliplications")
+    
+    #plt.legend(loc="lower right")
+    #plt.scatter(one)
+    #plt.scatter(factor)
+    #plt.scatter(conquer)
+    plt.show()
 #print(one)
 #print(factor)
 #print(conquer)
