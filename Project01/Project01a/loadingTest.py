b58b97baae8d154aba6aefa3333ec4cdee112e42
Amit Deb <deb@sonoma.edu>
2022-02-12 19:44:26 -0800

    Update loadingTest.py

--- Project01/Project01a/loadingTest.py
+++ Project01/Project01a/loadingTest.py
@@ -8,7 +8,7 @@ def setupToolbar():
     sys.stdout.flush()
     sys.stdout.write("\b" * (4+1)) # return to start of line, after '['
 
-amountOfFiles = 1000
+amountOfFiles = 300
 setupToolbar()
 
 percentage = 0
