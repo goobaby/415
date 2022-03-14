f2fe025697ec97e2d0aa760b1c2878b4b50e1eb3
Amit Deb <deb@sonoma.edu>
2022-03-14 16:39:15 -0700

    Rename

--- Project2b/main.js
+++ Project2b/main.js
@@ -5,7 +5,7 @@
  * @return {Number} Multiply of x and y
  */
  
- function karatsubaMulti(x, y) {
+ function karatsuba_mult(x, y) {
 
     let n = Math.min(('' + x).length, ('' + y).length);
   
@@ -20,18 +20,18 @@
     let b0 = parseInt(y / tenToHalf);
     let b1 = y % tenToHalf;
 
-    let c0 = karatsubaMulti(a0, b0);
-    let c2 = karatsubaMulti(a1, b1);
+    let c0 = karatsuba_mult(a0, b0);
+    let c2 = karatsuba_mult(a1, b1);
 
-    //let c1 = karatsubaMulti(a1 + a0, b1 + b0) - karatsubaMulti(a1, b1) - karatsubaMulti(a0, b0);
+    //let c1 = karatsuba_mult(a1 + a0, b1 + b0) - karatsuba_mult(a1, b1) - karatsuba_mult(a0, b0);
 
     let c3 = c2 + c0;
-    let c1 = karatsubaMulti(a1 + a0, b1 + b0) - c3;
+    let c1 = karatsuba_mult(a1 + a0, b1 + b0) - c3;
     //console.log(a0, a1, b0, b1);
   
     return tenToN * c0 + tenToHalf * c1 + c2
-    //return tenToN * c0 + tenToHalf * (karatsubaMulti(a1 + a0, b1 + b0) - c3) + c2;
-    //return tenToN * c0 + tenToHalf * (karatsubaMulti(a1 + a0, b1 + b0) - karatsubaMulti(a1, b1) - karatsubaMulti(a0, b0)) + c2;
+    //return tenToN * c0 + tenToHalf * (karatsuba_mult(a1 + a0, b1 + b0) - c3) + c2;
+    //return tenToN * c0 + tenToHalf * (karatsuba_mult(a1 + a0, b1 + b0) - karatsuba_mult(a1, b1) - karatsuba_mult(a0, b0)) + c2;
  }
 
- console.log(karatsubaMulti(2135, 4014));
+ console.log(karatsuba_mult(2135, 4014));
