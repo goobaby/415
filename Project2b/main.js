5bba5109a0d209a36351ee154e007847edeca690
Amit Deb <deb@sonoma.edu>
2022-03-16 23:36:37 -0700

    EVERYTHING IS FUCKED AH
    number needs to go into array help!

--- Project2b/main.js
+++ Project2b/main.js
@@ -8,41 +8,41 @@ window.onload = function() {
     var calculateButton = document.getElementById('calculateButton');
     calculateButton.addEventListener('click', finalDestination);
 
-    function karatsuba_mult(x, y) {
+    // function karatsuba_mult(x, y) {
 
-        let n = Math.min(('' + x).length, ('' + y).length);
-    
-        if(n == 1)
-            return x * y;
-    
-        let tenToHalf = Math.pow(10, parseInt(n / 2));
-        let tenToN = Math.pow(10, 2 * parseInt(n / 2));
-    
-        let a0 = parseInt(x / tenToHalf);
-        let a1 = x % tenToHalf;
-        let b0 = parseInt(y / tenToHalf);
-        let b1 = y % tenToHalf;
-
-        let c0 = karatsuba_mult(a0, b0);
-        let c2 = karatsuba_mult(a1, b1);
-
-        //let c1 = karatsuba_mult(a1 + a0, b1 + b0) - karatsuba_mult(a1, b1) - karatsuba_mult(a0, b0);
+    //     let n = Math.min(('' + x).length, ('' + y).length);
         
+    //     BigInt(n);
 
-        let c3 = c2 + c0;
-        let c1 = karatsuba_mult(a1 + a0, b1 + b0) - c3;
-        //console.log(a0, a1, b0, b1);
+    //     if(n == 1n)
+    //         return x * y;
     
-        return tenToN * c0 + tenToHalf * c1 + c2
-        //return tenToN * c0 + tenToHalf * (karatsuba_mult(a1 + a0, b1 + b0) - c3) + c2;
-        //return tenToN * c0 + tenToHalf * (karatsuba_mult(a1 + a0, b1 + b0) - karatsuba_mult(a1, b1) - karatsuba_mult(a0, b0)) + c2;
-    }
+    //     let tenToHalf = Math.pow(10, parseInt(n / 2));
+    //     let tenToN = Math.pow(10, 2 * parseInt(n / 2));
+    
+    //     let a0 = parseInt(x / tenToHalf);
+    //     let a1 = x % tenToHalf;
+    //     let b0 = parseInt(y / tenToHalf);
+    //     let b1 = y % tenToHalf;
 
+    //     let c0 = karatsuba_mult(a0, b0);
+    //     let c2 = karatsuba_mult(a1, b1);
 
-    //Can't return number but be returned as vector/array
+    //     //let c1 = karatsuba_mult(a1 + a0, b1 + b0) - karatsuba_mult(a1, b1) - karatsuba_mult(a0, b0);
+
+    //     let c3 = c2 + c0;
+    //     let c1 = karatsuba_mult(a1 + a0, b1 + b0) - c3;
+    //     //console.log(a0, a1, b0, b1);
+    
+    //     return tenToN * c0 + tenToHalf * c1 + c2
+    //     //return tenToN * c0 + tenToHalf * (karatsuba_mult(a1 + a0, b1 + b0) - c3) + c2;
+    //     //return tenToN * c0 + tenToHalf * (karatsuba_mult(a1 + a0, b1 + b0) - karatsuba_mult(a1, b1) - karatsuba_mult(a0, b0)) + c2;
+    // }
+
+    //Can't return number but be return array?
     function Exponentiation(a, n) {
         if (n == 0)
-            return 1;
+            return 1n;
         if ((n % 2 == 0) && (n > 0) )
             return karatsuba_mult(Exponentiation(a, n/2), Exponentiation(a, n/2));
         if (n % 2 == 1)
@@ -50,10 +50,14 @@ window.onload = function() {
     }
 
     function calcOne(x,y) { 
+        BigInt(x);
+        BigInt(y);
         calculated.value = karatsuba_mult(x, y);
     }
 
     function calcTwo(x,y) {
+        BigInt(x);
+        BigInt(y);
         calculated.value = Exponentiation(x, y);
     }
 
