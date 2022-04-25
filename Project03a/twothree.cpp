70825c21ebed8d5e66ef27baeb52f1be4885aa45
Amit Deb <deb@sonoma.edu>
2022-04-24 18:26:40 -0700

    Create twothree.cpp

---
+++ Project03a/twothree.cpp
@@ -0,0 +1,13 @@
+//Prototypes for the 2-3 tree class
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class twoThree{
+    public:
+        twoThree();
+}
